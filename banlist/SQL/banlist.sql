2b51a93f495c4e3360b85cad3d4d15445f9e9cac
CzechDEVStation <77108702+CzechDEVStation@users.noreply.github.com>
2021-01-08 17:35:06 +0100

    Update banlist.sql
    Nové identifiery.

--- banlist/SQL/banlist.sql
+++ banlist/SQL/banlist.sql
@@ -10,59 +10,57 @@ CREATE TABLE `banlist` (
 );
 
 INSERT INTO `banlist` (`identifier1`, `identifier2`, `identifier3`, `identifier4`, `identifier5`, `identifier6`) VALUES
-
-	('Identifier1', 'Identifier2', 'Identifier3', 'Identifier4', 'Identifier5', 'Identifier6'),
-	('steam:11000010214d4bb', 'license:540eec71f769e143f0a1f3a1637ce53acbda056b', 'no info', 'no info', 'no info', 'ip:37.221.242.248'),
+	('steam:11000010214d4bb', 'license:540eec71f769e143f0a1f3a1637ce53acbda056b', 'unknown', 'unknown', 'unknown', 'ip:37.221.242.248'),
 
 	('steam:110000104676ef0', 'license:434fb18c37d87867e0103df47ed154386a963942', 'live:1055521889207525', 'xbl:2535432494287938', 'discord:409284020313128960', 'ip:185.115.9.221'),
 
-	('steam:110000104ff2d86', 'license:a41b296e6ea800ec8cb90569c0d21e37b4fb49cd', 'live:985154183993094', 'xbl:2535461350381214', 'discord:283687658092232706', 'no info'),
+	('steam:110000104ff2d86', 'license:a41b296e6ea800ec8cb90569c0d21e37b4fb49cd', 'live:985154183993094', 'xbl:2535461350381214', 'discord:283687658092232706', 'unknown'),
 
 	('steam:11000010698b566', 'license:cf0c183cb8779f302f7096b06ea055efe4c6e278', 'live:985156796902172', 'xbl:2533274941741233', 'discord:544966450503483392', 'ip:185.91.165.95'),
 
-	('steam:11000010703a879', 'license:9a6ea20415b30a11d0d9a2906eac62bdd3829274', 'no info', 'no info', 'discord:321697349854298112', 'ip:89.24.43.5'),
+	('steam:11000010703a879', 'license:9a6ea20415b30a11d0d9a2906eac62bdd3829274', 'unknown', 'unknown', 'discord:321697349854298112', 'ip:89.24.43.5'),
 
 	('steam:1100001070797c1', 'license:f77362502106add2e469e2ef5dd4196ab016cef7', 'live:1829582785882805', 'xbl:2535469532452317', 'discord:313418282868801536', 'ip:176.12.125.123'),
 
 	('steam:110000108c9f0e8', 'license:4d0a61e47c376840943da83a883e3c9903c04564', 'live:985157245820988', 'xbl:2535439934045185', 'discord:394184522821795852', 'ip:46.13.188.241'),
 
-	('steam:11000010a80019a', 'license:b631f42f105474545597919770c7b36c53dc2419', 'no info', 'no info', 'no info', 'ip:93.153.36.57'),
+	('steam:11000010a80019a', 'license:b631f42f105474545597919770c7b36c53dc2419', 'unknown', 'unknown', 'unknown', 'ip:93.153.36.57'),
 
 	('steam:11000010e82835d', 'license:7f94b7bc2fc119c6a0c53149c43539cb8e419efe', 'live:985157307205576', 'xbl:2535470001705080', 'discord:422084212586315797', 'ip:31.30.175.161'),
 
 	('steam:110000110ee1c6c', 'license:2f515df101729f2ad621a9b1eadff45d6b4838ae', 'live:844425129363198', 'xbl:2535447846078741', 'discord:309685107609042945', 'ip:89.177.204.59/'),
 
-	('steam:1100001119f7c02', 'license:87115d1538638cf3cb52c8b10f1b7550c2b6a7ab', 'no info', 'no info', 'discord:561967492981981185', 'ip:80.243.105.55'),
+	('steam:1100001119f7c02', 'license:87115d1538638cf3cb52c8b10f1b7550c2b6a7ab', 'unknown', 'unknown', 'discord:561967492981981185', 'ip:80.243.105.55'),
 
-	('steam:110000112918239', 'license:4b760fa4384cf9d3b346e51a473e0199d25a07a9', 'live:914798058864316', 'xbl:2535422518902530', 'no info', 'ip:85.237.234.180'),
+	('steam:110000112918239', 'license:4b760fa4384cf9d3b346e51a473e0199d25a07a9', 'live:914798058864316', 'xbl:2535422518902530', 'unknown', 'ip:85.237.234.180'),
 
-	('steam:1100001176eceef', 'license:cb83f8e15836bf2053cecd25ef8b85b598c136d9', 'live:985154519471039', 'xbl:2535444430762569', 'no info', 'ip:89.102.157.154'),
+	('steam:1100001176eceef', 'license:cb83f8e15836bf2053cecd25ef8b85b598c136d9', 'live:985154519471039', 'xbl:2535444430762569', 'unknown', 'ip:89.102.157.154'),
 
 	('steam:1100001180af948', 'license:1711d354eb3fde5456558b216b4c897e8d424a7d', 'live:844424968772106', 'xbl:2535445343774395', 'discord:411601991681441797', 'ip:178.143.40.60'),
 
 	('steam:1100001186cdec1', 'license:9778ca3cfffaef9ef4894cddd5a227b2b5117a41', 'live:985153959384482', 'xbl:2535415356508370', 'discord:453667977900851211', 'ip:84.192.246.88'),
 
-	('steam:11000011933f52d', 'license:7a0437132f149f59c13dc38c5776ca3584bab74d', 'live:1055518405598977', 'xbl:2535424884895871', 'no info', 'ip:91.225.135.35'),
+	('steam:11000011933f52d', 'license:7a0437132f149f59c13dc38c5776ca3584bab74d', 'live:1055518405598977', 'xbl:2535424884895871', 'unknown', 'ip:91.225.135.35'),
 
-	('steam:11000011a38bf26', 'license:4dc32f681ea5d0da532e54ee7bc6fe67130be957', 'live:1055518841266558', 'no info', 'no info', 'ip:185.216.167.2'),
+	('steam:11000011a38bf26', 'license:4dc32f681ea5d0da532e54ee7bc6fe67130be957', 'live:1055518841266558', 'unknown', 'unknown', 'ip:185.216.167.2'),
 
-	('steam:110000132f995e3', 'license:4952f049a17fc96c88163f561d872e2f27bff310', 'no info', 'no info', 'discord:408228744109621258', 'ip:194.228.11.204'),
+	('steam:110000132f995e3', 'license:4952f049a17fc96c88163f561d872e2f27bff310', 'unknown', 'unknown', 'discord:408228744109621258', 'ip:194.228.11.204'),
 
-	('steam:11000013345e7ea', 'license:3b13e6cba372df5b298461f887ed82628628a7bc', 'no info', 'xbl:2535451110436942', 'discord:332547782957203456', 'ip:78.156.32.246'),
+	('steam:11000013345e7ea', 'license:3b13e6cba372df5b298461f887ed82628628a7bc', 'unknown', 'xbl:2535451110436942', 'discord:332547782957203456', 'ip:78.156.32.246'),
 
-	('steam:1100001347a38fc', 'license:bbc4b7979a8d04449428838874b3e80ba83c1a0a', 'live:1688853909758562', 'xbl:2535470204520590', 'no info', 'ip:217.138.199.165'),
+	('steam:1100001347a38fc', 'license:bbc4b7979a8d04449428838874b3e80ba83c1a0a', 'live:1688853909758562', 'xbl:2535470204520590', 'unknown', 'ip:217.138.199.165'),
 
-	('steam:11000013606a63e', 'license:0a6325dd677cdbb81ce171cd668159906e9f8566', 'no info', 'no info', 'discord:497801053014720512', 'ip:194.228.59.106'),
+	('steam:11000013606a63e', 'license:0a6325dd677cdbb81ce171cd668159906e9f8566', 'unknown', 'unknown', 'discord:497801053014720512', 'ip:194.228.59.106'),
 
 	('steam:11000013616df59', 'license:c49248c3050c44e8ad56434ccd215a659722605d', 'live:985156961116302', 'xbl:2535465641232858', 'discord:352190836462714880', 'ip:37.48.59.141'),
 
-	('steam:1100001371fe919', 'license:7392d3f4e4a070b91ec48ec0d83955fbacaba5ba', 'no info', 'no info', 'discord:315204529886986243', 'ip:109.81.213.208'),
+	('steam:1100001371fe919', 'license:7392d3f4e4a070b91ec48ec0d83955fbacaba5ba', 'unknown', 'unknown', 'discord:315204529886986243', 'ip:109.81.213.208'),
 
 	('steam:11000013bc3f444', 'license:7e199f6c4b5dfeeaf79dfa96b6e526bf18118a55', 'live:1055522000477433', 'xbl:2535430131026784', 'discord:299885736881029131', 'ip:89.177.72.150'),
 
-	('steam:11000013ca38219', 'license:39d870d291c4879bb5135e7043b14c7d3b9686cd', 'live:1055518768379381', 'xbl:2535444408076299', 'no info', 'ip:85.70.250.68'),
+	('steam:11000013ca38219', 'license:39d870d291c4879bb5135e7043b14c7d3b9686cd', 'live:1055518768379381', 'xbl:2535444408076299', 'unknown', 'ip:85.70.250.68'),
 
-	('steam:110000105a9cc04', 'license:ddc118649093ec5425619cb73d6bcf748ce102a1', 'no info', 'no info', 'discord:632317344676315138', 'ip:89.102.18.98'),
+	('steam:110000105a9cc04', 'license:ddc118649093ec5425619cb73d6bcf748ce102a1', 'unknown', 'unknown', 'discord:632317344676315138', 'ip:89.102.18.98'),
 
 	('steam:110000109a06fd9', 'license:4e0d024d598036d8f477e5a87a87ca96ace66bb8', 'live:844425200617965', 'xbl:2535405239220957', 'discord:423121053770842122', 'ip:82.117.136.226'),
 
@@ -70,57 +68,103 @@ INSERT INTO `banlist` (`identifier1`, `identifier2`, `identifier3`, `identifier4
 
 	('steam:11000010e766a5c', 'license:2be6347d8c5bf4d2e498312b2af73f1d03867259', 'live:1055518437752305', 'xbl:2535415166601864', 'discord:429000440965759016', 'ip:81.190.147.12'),
 
-	('steam:110000112c7bf1d', 'license:9dc20ff0acc76a0158457ce006ecb58935cb6f69', 'live:1055518573035830', 'no info', 'discord:487642963896172555', 'ip:95.102.128.191'),
+	('steam:110000112c7bf1d', 'license:9dc20ff0acc76a0158457ce006ecb58935cb6f69', 'live:1055518573035830', 'unknown', 'discord:487642963896172555', 'ip:95.102.128.191'),
 
 	('steam:110000116226787', 'license:f3eeb5f4dc4c7fa1014ee9e95e02350977221490', 'live:844429175143267', 'xbl:2535461271138869', 'discord:391809353553936395', 'ip:92.79.32.212'),
 
 	('steam:11000011678bfd3', 'license:6920436f0fb40a98302935f3849915661e702f80', 'live:844425087717291', 'xbl:2535448022513247', 'discord:523859611703574528', 'ip:185.142.48.53'),
 
-	('steam:110000118ed3527', 'license:18f7b5b78db0453159d9585fd29e7ff9d82c6427', 'no info', 'no info', 'discord:606089022216863754', 'ip:90.178.248.11'),
+	('steam:110000118ed3527', 'license:18f7b5b78db0453159d9585fd29e7ff9d82c6427', 'unknown', 'unknown', 'discord:606089022216863754', 'ip:90.178.248.11'),
 
 	('steam:110000119a1f0ab', 'license:c924167b3d4c8620acd02e9d7823bf2b867a6ced', 'live:985154026362604', 'xbl:2535413581674522', 'discord:446325753580093440', 'ip:94.136.151.38'),
 
 	('steam:1100001339b0cef', 'license:b7a3a66681119b259199104fabf80b8830197d6c', 'live:844428550570129', 'xbl:2535419560497141', 'discord:380088527419080704', 'ip:86.49.238.249'),
 
-	('steam:11000013503b53c', 'license:4d6ddab284040919ee60155ed909fa47f80bea64', 'no info', 'no info', 'discord:368192659648413698', 'ip:46.121.142.169'),
+	('steam:11000013503b53c', 'license:4d6ddab284040919ee60155ed909fa47f80bea64', 'unknown', 'unknown', 'discord:368192659648413698', 'ip:46.121.142.169'),
 
 	('steam:110000135341c9e', 'license:5e4945a8ed574df39f52b2ca258ee148d15bf0f1', 'live:985157358540733', 'xbl:2535468986647023', 'discord:516279904128729117', 'ip:85.163.73.115'),
 
-	('steam:110000137e1e60d', 'license:01c23b6452a2123bb0c9cefe044f0fcb521ccc58', 'no info', 'no info', 'no info', 'ip:83.21.92.117'),
+	('steam:110000137e1e60d', 'license:01c23b6452a2123bb0c9cefe044f0fcb521ccc58', 'unknown', 'unknown', 'unknown', 'ip:83.21.92.117'),
 
-	('steam:11000013d5775ce', 'license:d25095a288da1d39420e34acdb10475b39f1bd02', 'no info', 'no info', 'no info', 'ip:194.228.13.41'),
+	('steam:11000013d5775ce', 'license:d25095a288da1d39420e34acdb10475b39f1bd02', 'unknown', 'unknown', 'unknown', 'ip:194.228.13.41'),
 
-	('steam:11000013e76d70b', 'license:2dfdb6d39fec5e7bd7588c8cbf2720cfba4e1132', 'no info', 'no info', 'no info', 'ip:89.177.9.96'),
+	('steam:11000013e76d70b', 'license:2dfdb6d39fec5e7bd7588c8cbf2720cfba4e1132', 'unknown', 'unknown', 'unknown', 'ip:89.177.9.96'),
 
 	('steam:110000119b2bd2e', 'license:ac4187afca7964c933697f0e75c425e777e9a6ed', 'live:914800724259380', 'xbl:2533274991822783', 'discord:544473598136418304', 'ip:185.191.206.4'),
 
-	('steam:11000013d1a5762', 'license:cdcdb357722b7ee06cb0218bef146c7fd050efc2', 'no info', 'no info', 'discord:563337935898804234', 'ip:36.232.228.104'),
+	('steam:11000013d1a5762', 'license:cdcdb357722b7ee06cb0218bef146c7fd050efc2', 'unknown', 'unknown', 'discord:563337935898804234', 'ip:36.232.228.104'),
 
-	('steam:11000010b4e493b', 'license:d105a018cf15a80ff0d65d2e9aaf4187848e720c', 'live:1899947186951110', 'no info', 'no info', 'ip:80.61.37.86'),
+	('steam:11000010b4e493b', 'license:d105a018cf15a80ff0d65d2e9aaf4187848e720c', 'live:1899947186951110', 'unknown', 'unknown', 'ip:80.61.37.86'),
 
-	('steam:11000010b8b4b8d', 'license:af4565cd56ac82727ec00e48c616aedeae258a67', 'no info', 'no info', 'discord:354985213840850956', 'ip:213.226.239.11'),
+	('steam:11000010b8b4b8d', 'license:af4565cd56ac82727ec00e48c616aedeae258a67', 'unknown', 'unknown', 'discord:354985213840850956', 'ip:213.226.239.11'),
 
 	('steam:1100001166cbf31', 'license:1357eada22e6f98aa692b2d7617c22bed50f6dc9', 'live:985157348903525', 'xbl:2535430140909748', 'discord:398856430079311883', 'ip:185.113.32.216'),
 
-	('steam:110000140c777ac', 'license:9c86aa851e25de4918044fa05e852213af6f4d7d', 'no info', 'no info', 'no info', 'ip:86.106.90.86'),
+	('steam:110000140c777ac', 'license:9c86aa851e25de4918044fa05e852213af6f4d7d', 'unknown', 'unknown', 'unknown', 'ip:86.106.90.86'),
 
-	('steam:1100001418e4db7', 'license:4162e312a502445b5c6bbc8128eda8092e95b11e', 'no info', 'no info', 'no info', 'ip:89.24.44.165'),
+	('steam:1100001418e4db7', 'license:4162e312a502445b5c6bbc8128eda8092e95b11e', 'unknown', 'unknown', 'unknown', 'ip:89.24.44.165'),
 
-	('steam:110000141f7f2fc', 'license:f31b57e7b912fb9420ea49d3d0e46d62154c738c', 'live:914798649441980', 'no info', 'no info', 'ip:217.138.199.52'),
+	('steam:110000141f7f2fc', 'license:f31b57e7b912fb9420ea49d3d0e46d62154c738c', 'live:914798649441980', 'unknown', 'unknown', 'ip:217.138.199.52'),
 
-	('steam:110000113d90e91', 'license:5ce5bc2a831df49e415c581d408ca23d99b82e5d', 'no info', 'no info', 'discord:342362224846831626', 'ip:78.80.229.4'),
+	('steam:110000113d90e91', 'license:5ce5bc2a831df49e415c581d408ca23d99b82e5d', 'unknown', 'unknown', 'discord:342362224846831626', 'ip:78.80.229.4'),
 
-	('steam:1100001180af948', 'license:1711d354eb3fde5456558b216b4c897e8d424a7d', 'no info', 'no info', 'no info', 'ip:178.143.40.115'),
+	('steam:1100001180af948', 'license:1711d354eb3fde5456558b216b4c897e8d424a7d', 'unknown', 'unknown', 'unknown', 'ip:178.143.40.115'),
 
-	('steam:11000043eefecd0', 'license:a250a088db5c6b0f7da6041e939289b1cdaad0a4', 'live:1055518968343180', 'no info', 'no info', 'ip: 79:141.242.7'),
+	('steam:11000043eefecd0', 'license:a250a088db5c6b0f7da6041e939289b1cdaad0a4', 'live:1055518968343180', 'unknown', 'unknown', 'ip: 79:141.242.7'),
 
-	('steam:11000014267d316', 'license:67945a1602f885908e65ff2589747a8c3c369098', 'no info', 'no info', 'no info', 'ip:185.242.6.6'),
+	('steam:11000014267d316', 'license:67945a1602f885908e65ff2589747a8c3c369098', 'unknown', 'unknown', 'unknown', 'ip:185.242.6.6'),
 
-	('steam:110000117eccbb5', 'license:d332e9da3e46a3b623e140a38f604c0c81989eea', 'no info', 'no info', 'no info', 'ip:37.188.173.212'),
+	('steam:110000117eccbb5', 'license:d332e9da3e46a3b623e140a38f604c0c81989eea', 'unknown', 'unknown', 'unknown', 'ip:37.188.173.212'),
 
-	('steam:11000011654724d', 'license:9b088c3433487984fe32afb5f0a0552512a991cc', 'live:844424944508663', 'xbl:2535415776063574', 'no info', 'ip:178.255.168.196'),
+	('steam:11000011654724d', 'license:9b088c3433487984fe32afb5f0a0552512a991cc', 'live:844424944508663', 'xbl:2535415776063574', 'unknown', 'ip:178.255.168.196'),
 
-	('steam:110000143a426f9', 'license:945996fcf541c5de5a86a98d0e3cdd72056f4c8', 'no info', 'no info', 'no info', 'no info'),
+	('steam:110000143a426f9', 'license:945996fcf541c5de5a86a98d0e3cdd72056f4c8', 'unknown', 'unknown', 'unknown', 'unknown'),
 	
-	('steam:1100001378ca562', 'no info', 'no info', 'no info', 'no info', 'no info'),
+	('steam:1100001378ca562', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000013cea1d22', 'license:8660491071ea545808f35a256ffcf5007740757f', 'unknown', 'unknown', 'unknown', 'ip:78.102.73.57'),
+
+	('steam:11000013fddbb4a', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:110000119849b78', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:1100001121d5b73', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000010689f0b4', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:110000117904acf', 'license:a0b3fec7961c06db42eb6589607642558ee694cf', 'live:914798403117629', 'unknown', 'discord:277779106471608320', 'ip:62.245.67.163'),
+
+	('steam:1100001418e4db7', 'license:4162e312a502445b5c6bbc8128eda8092e95b11e', 'unknown', 'unknown', 'unknown', 'ip:89.24.44.165'),
+
+	('steam:11000013be726c0', 'license:4c47bad4d4fc3f358080b3479dedff450f708397', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000010a80019a', 'license:7ab8d6aadb84352e8876cda99187c8f72816223a', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000013629636a', 'license:f4b6d613800b760c09c96d0b0d59ee5dce2416b4', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000013ba59799', 'license:7f181c30c308b41f9df080aea782b32d47d7bde1', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000011555fbcd', 'license:a1015021e097afef5b02ee93d073542c9bbf537c', 'live:844424934509337', 'xbl:2535457296713155', 'discord:493419971913842729', 'ip:195.68.234.94'),
+
+	('steam:1100001167cb33f', 'license:32067fc510b20b4601275a7990301943d7da794a', 'live:985154115962702', 'unknown', 'unknown', 'ip:78.45.239.52'),
+
+	('steam:1100001418bde4b', 'license:e332884622bd6d5703561365e3c71927ca7edfdc', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000014245e19f', 'license:41efbe5b80d1197997e64ac878c5ff686ca7f6bf', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:11000013b709e4e', 'license:0fd8006986288f1892f1aa98d61be4437c999ee0', 'unknown', 'unknown', 'discord:543102989003522080', 'ip:37.221.242.242'),
+
+	('steam:1100001071e3145', 'license:f6c888e51b3f70a202bcc7659d4d4e5d338f779c', 'unknown', 'unknown', 'discord:2390611325001523200', 'ip:89.24.58.64'),
+
+	('steam:110000112913033', 'license:c1e98f758bd724e75ee9815a62baacf215796a7a', 'unknown', 'unknown', 'unknown', 'ip:93.91.150.113'),
+
+	('steam:110000141e94563', 'license:a46cee8a60b1868b20230ab442aa0bbd09fe22f1', 'live:1055518387061957', 'xbl:2535469380075954', 'unknown', 'ip:188.123.105.10'),
+
+	('steam:110000132b34805', 'license:43e5122c85bf6bb3f1faa73db8df533903cae765', 'live:985154016506741', 'xbl:2535448677666519', 'unknown', 'ip:193.165.236.233'),
+
+	('steam:110000140b758e4', 'license:1451c38baf3d8d2d2b6e1a0a307aa4f246278278', 'live:1055518313272980', 'xbl:2535444632679456', 'discord:721838273005224011', 'ip:93.99.149.217'),
+
+	('steam:11000011d1d5e42', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'),
+
+	('steam:110000114fa77f3', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown')
 ;
