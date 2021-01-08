5040268970629fe293ffd1dfb2a13fd2239e0a62
CzechDEVStation <77108702+CzechDEVStation@users.noreply.github.com>
2021-01-08 17:36:01 +0100

    Update fxmanifest.lua

--- banlist/fxmanifest.lua
+++ banlist/fxmanifest.lua
@@ -2,16 +2,12 @@ fx_version 'bodacious'
 
 game 'gta5'
 
-free_resource 'Yes'
-
 author 'Ghast#4251'
 
-description 'Please do not ressel this resource.'
+description 'Please do not sell this resource.'
 
 download 'TODO'
 
 server_only 'yes'
 
 server_script 'server/main.lua'
-
--- Toto je free resource najdete ho na: TODO
