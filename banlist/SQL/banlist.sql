d33671db915666856f4283a0be6babaf6bd9fa5d
CzechDEVStation <77108702+CzechDEVStation@users.noreply.github.com>
2021-01-19 18:50:25 +0100

    Update banlist.sql

--- banlist/SQL/banlist.sql
+++ banlist/SQL/banlist.sql
@@ -170,5 +170,7 @@ INSERT INTO `banlist` (`identifier1`, `identifier2`, `identifier3`, `identifier4
 
 	('steam:11000014414a2c1', 'unknown', 'unknown', 'unknown', 'discord:653518197810790421', 'unknown'),
 	
-	('unknown', 'unknown', 'unknown', 'unknown', 'discord:513775528386494474', 'unknown')
+	('unknown', 'unknown', 'unknown', 'unknown', 'discord:513775528386494474', 'unknown'),
+
+	('unknown', 'unknown', 'unknown', 'unknown', 'discord:543520567219847179', 'unknown')
 ;
